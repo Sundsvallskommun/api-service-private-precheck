cb44d79558068b9644afda855a4605aa48da4987
Staticalpig <elinus@live.se>
2023-09-29 14:11:18 +0200

    Fixed .env support for importing of test api url

--- src/main/java/se/sundsvall/precheck/service/PrecheckService.java
+++ src/main/java/se/sundsvall/precheck/service/PrecheckService.java
@@ -1,6 +1,10 @@
 package se.sundsvall.precheck.service;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.core.env.Environment;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 import se.sundsvall.precheck.api.PecheckResource;
 import se.sundsvall.precheck.api.model.PrecheckResponse;
@@ -9,12 +13,16 @@ import java.util.List;
 
 @Service
 public class PrecheckService {
+/*
+        static String foo;
+        //spring.test.url
 
-        @Value("${spring.application.url}")
-        private String url;
-        public List<PrecheckResponse> checkPermit(String partyId, String assetType, String municipalityId) {
-                //print application.properties url to console
-                System.out.println("url: " + url);
+        public PrecheckService(@Value("${aaaa}") String foo) {
+                this.foo = foo; //foo is now aaaa from .env file (After 10 minutes of debugging :) )
+        }*/
+
+
+        public static List<PrecheckResponse> checkPermit(String partyId, String assetType, String municipalityId) {
 
                 return List.of(
                         PrecheckResponse.builder()
