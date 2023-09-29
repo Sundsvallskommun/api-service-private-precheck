1182dc9cae6c6de41982daa31679355008f2a7a9
Staticalpig <elinus@live.se>
2023-09-29 14:11:18 +0200

    Fixed .env support for importing of test api url

--- src/main/java/se/sundsvall/precheck/api/model/AcctivePermitsResponse.java
+++ src/main/java/se/sundsvall/precheck/api/model/AcctivePermitsResponse.java
@@ -1,4 +1,21 @@
 package se.sundsvall.precheck.api.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor(access = lombok.AccessLevel.PRIVATE)
+@Builder(setterPrefix = "with")
 public class AcctivePermitsResponse {
+
+    @Schema(description = "The type of asset that are acctive", example = "String")
+    private List<Permit> permits;
+
 }
+
+
+
