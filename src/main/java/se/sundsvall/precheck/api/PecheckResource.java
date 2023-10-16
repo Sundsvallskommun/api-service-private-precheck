1f0d15d425711ec697b1ad8718012b1fd399691a
Staticalpig <elinus@live.se>
2023-10-16 12:10:45 +0200

    Removed Validated and changed it to ValidUuid

--- src/main/java/se/sundsvall/precheck/api/PecheckResource.java
+++ src/main/java/se/sundsvall/precheck/api/PecheckResource.java
@@ -1,13 +1,10 @@
 package se.sundsvall.precheck.api;
 
 
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-import static org.springframework.http.MediaType.APPLICATION_PROBLEM_JSON_VALUE;
-import static org.springframework.http.ResponseEntity.ok;
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.validation.annotation.Validated;
@@ -15,17 +12,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import io.swagger.v3.oas.annotations.media.Schema;
-import se.sundsvall.precheck.Application;
+import se.sundsvall.dept44.common.validators.annotation.ValidUuid;
 import se.sundsvall.precheck.api.model.PrecheckResponse;
-import se.sundsvall.precheck.api.model.PermitListResponse;
 import se.sundsvall.precheck.service.PrecheckService;
 
 import java.util.logging.Logger;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.http.MediaType.APPLICATION_PROBLEM_JSON_VALUE;
+
 
 @RestController
-@Validated
+@ValidUuid
 @RequestMapping("/precheck")
 @Tag(name = "precheck", description = "The precheck API for citizens and CitizenAssets")
 public class PecheckResource {
@@ -49,9 +47,6 @@ public class PecheckResource {
                 @Parameter(name = "assetType", description = "AssetType for the citizen", example = "PARKING_PERMIT") final String assetType,
                 @Parameter(name = "municipalityId", description = "MunicipalityId for the citizen", example = "2555") final int municipalityId
         ) {
-                LOG.info("partyId: " + partyId + " assetType: " + assetType + " municipalityId: " + municipalityId);
-
                 return precheckService.checkPermit(partyId, assetType, municipalityId);
-
         }
 }
