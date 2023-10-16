1480ac1b2545af42858a965b9e94c89ef8873303
Staticalpig <elinus@live.se>
2023-10-16 12:28:23 +0200

    Fixed PrecheckResponse examples and removed initaliation on String reason

--- src/main/java/se/sundsvall/precheck/api/model/PrecheckResponse.java
+++ src/main/java/se/sundsvall/precheck/api/model/PrecheckResponse.java
@@ -9,14 +9,14 @@ import lombok.Getter;
 @AllArgsConstructor(access = lombok.AccessLevel.PRIVATE)
 @Builder(setterPrefix = "with")
 public class PrecheckResponse {
-    @Schema(description = "The Status code", example = "int")
+    @Schema(description = "The Status code", example = "200")
     private int Status;
-    @Schema(description = "The type of asset", example = "String")
+    @Schema(description = "The type of asset", example = "PARKING_PERMIT")
     private String assetType;
     @Schema(description = "Orderable", example = "boolean")
     private boolean orderable = false;
     @Schema(description = "The reason if Orderable if false ", example = "String")
-    private String reason = "";
+    private String reason;
 
     public PrecheckResponse from(final String assetType, final boolean orderable, final String reason) {
         return builder()
