3a0edf757c704bfed639e2c8c017ac9c602143f3
Staticalpig <elinus@live.se>
2023-12-06 12:29:34 +0100

    Small PR feedback spelling fix and removal of String.format

--- src/main/java/se/sundsvall/precheck/constant/Constants.java
+++ src/main/java/se/sundsvall/precheck/constant/Constants.java
@@ -5,9 +5,9 @@ public class Constants {
     public static final String INVALID_MUNICIPALITY_ID = "The owner of the partyId '%s' is not registered in the municipality where the permit is requested";
 
     // Used in PreCheckUtil.java
-    public static final String NOT_FOUND_ERROR_MESSAGE = "Citizen data for '%s' was not given or had no content during execution of the service";
+    public static final String NOT_FOUND_ERROR_MESSAGE = "Citizen data for '%s' was not given or had no content during execution";
     public static final String CORRECT_ADDRESS_TYPE = "POPULATION_REGISTRATION_ADDRESS";
-    public static final String NO_VALID_MUNICIPALITY_ID_FOUND = "The owner of the partyId given is not register in the municipality where the permit is requested";
+    public static final String NO_VALID_MUNICIPALITY_ID_FOUND = "The owner of the given partyId is not register in the municipality where the permit is requested";
     public static final String ASSET_TYPE_EXISTS_ERROR_MESSAGE = "PersonId already has a permit of type '%s' associated with it";
 
 
